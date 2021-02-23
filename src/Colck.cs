2e72b53c3b6e91d660e668ae594cd2d9bf772fad
Connor Slade <turtleslade@gmail.com>
2021-02-22 22:26:44 -0500

    Min done!

---
+++ src/Colck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VirtualTixClock
+{
+    class Clock
+    {
+        public static char GetTimeChar(string timeType, int index)
+        {
+            var date = DateTime.Now.ToString(timeType);
+            return date[index];
+        }
+    }
+}
--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,28 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace VirtualTixClock
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private DispatcherTimer DispatcherTimer;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer = new DispatcherTimer();
+            DispatcherTimer.Tick += DispatcherTimer_Tick;
+            DispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            DispatcherTimer.Start();
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            var AllTxt = new List<Rectangle> { C0, C1, C2, C3, C4, C5, D0, D1, D2, D3, D4, D5, D6, D7, D8 };
+            var MinTix1 = new List<Rectangle> { D0, D1, D2, D3, D4, D5, D6, D7, D8 };
+            var MinTix2 = new List<Rectangle> { C0, C1, C2, C3, C4, C5 };
+
+            var Second = int.Parse(Clock.GetTimeChar("mm", 1).ToString(), 0);
+            var Minute = int.Parse(Clock.GetTimeChar("mm", 0).ToString(), 0);
+
+            Debug.Text = Minute.ToString();
+            clearTix(AllTxt);
+
+            var usedRect = new List<Rectangle> { };
+            for (var i = 0; i < Minute; i++)
+            {
+                RectSet(usedRect, MinTix2, 6);
+            }
+
+            usedRect = new List<Rectangle> { };
+            for (var i = 0; i < Second; i++)
+            {
+                RectSet(usedRect, MinTix1, 9);
+            }
+        }
+
+        private void RectSet(List<Rectangle> usedRect, List<Rectangle> MinTix, int max)
+        {
+            var Rnd = new Random();
+            
+                var rect = MinTix[Rnd.Next(0, max)];
+                if (usedRect.Contains(rect))
+                {
+                    RectSet(usedRect, MinTix, max);
+                }
+                else
+                {
+                    rect.Visibility = Visibility.Visible;
+                    usedRect.Add(rect);
+                }
+        }
+
+        private void clearTix(List<Rectangle> index)
+        {
+            foreach (var rect in index)
+            {
+                rect.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
