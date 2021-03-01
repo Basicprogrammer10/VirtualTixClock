18baa624fba701cbde265d7d1d751365f5ceb1ee
Connor Slade <turtleslade@gmail.com>
2021-03-01 16:51:08 -0500

    a thing

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ namespace VirtualTixClock
 {
     public partial class MainWindow
     {
-        private DispatcherTimer DispatcherTimer;
+        private DispatcherTimer _dispatcherTimer;
 
         public MainWindow()
         {
@@ -21,63 +21,63 @@ namespace VirtualTixClock
             Topmost = true;
             DispatcherTimer_Tick(new object(), new EventArgs());
 
-            DispatcherTimer = new DispatcherTimer();
-            DispatcherTimer.Tick += DispatcherTimer_Tick;
-            DispatcherTimer.Interval = new TimeSpan(0, 0, 5);
-            DispatcherTimer.Start();
+            _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += DispatcherTimer_Tick;
+            _dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
+            _dispatcherTimer.Start();
         }
 
         private void DispatcherTimer_Tick(object sender, EventArgs e)
         {
-            var AllTxt = new List<Rectangle> {A0, A1, A2, B0, B1, B2, B3, B4, B5, B6, B7, B8, C0, C1, C2, C3, C4, C5, D0, D1, D2, D3, D4, D5, D6, D7, D8};
-            var HorTix1 = new List<Rectangle> {A0, A1, A2};
-            var HorTix2 = new List<Rectangle> {B0, B1, B2, B3, B4, B5, B6, B7, B8};
-            var MinTix1 = new List<Rectangle> {D0, D1, D2, D3, D4, D5, D6, D7, D8};
-            var MinTix2 = new List<Rectangle> {C0, C1, C2, C3, C4, C5};
+            var allTxt = new List<Rectangle> {A0, A1, A2, B0, B1, B2, B3, B4, B5, B6, B7, B8, C0, C1, C2, C3, C4, C5, D0, D1, D2, D3, D4, D5, D6, D7, D8};
+            var horTix1 = new List<Rectangle> {A0, A1, A2};
+            var horTix2 = new List<Rectangle> {B0, B1, B2, B3, B4, B5, B6, B7, B8};
+            var minTix1 = new List<Rectangle> {D0, D1, D2, D3, D4, D5, D6, D7, D8};
+            var minTix2 = new List<Rectangle> {C0, C1, C2, C3, C4, C5};
 
-            var Minute = int.Parse(Clock.GetTimeChar("mm", 0).ToString(), 0);
-            var Minute2 = int.Parse(Clock.GetTimeChar("mm", 1).ToString(), 0);
-            var Hour = int.Parse(Clock.GetTimeChar("HH", 0).ToString(), 0);
-            var Hour2 = int.Parse(Clock.GetTimeChar("HH", 1).ToString(), 0);
+            var minute = int.Parse(Clock.GetTimeChar("mm", 0).ToString(), 0);
+            var minute2 = int.Parse(Clock.GetTimeChar("mm", 1).ToString(), 0);
+            var hour = int.Parse(Clock.GetTimeChar("HH", 0).ToString(), 0);
+            var hour2 = int.Parse(Clock.GetTimeChar("HH", 1).ToString(), 0);
 
-            ClearTix(AllTxt);
+            ClearTix(allTxt);
 
-            var UsedRect = new List<Rectangle>();
-            for (var I = 0; I < Minute; I++)
-                RectSet(UsedRect, MinTix2, 6);
+            var usedRect = new List<Rectangle>();
+            for (var I = 0; I < minute; I++)
+                RectSet(usedRect, minTix2, 6);
 
-            UsedRect = new List<Rectangle>();
-            for (var I = 0; I < Minute2; I++)
-                RectSet(UsedRect, MinTix1, 9);
+            usedRect = new List<Rectangle>();
+            for (var I = 0; I < minute2; I++)
+                RectSet(usedRect, minTix1, 9);
 
-            UsedRect = new List<Rectangle>();
-            for (var I = 0; I < Hour; I++)
-                RectSet(UsedRect, HorTix1, 3);
+            usedRect = new List<Rectangle>();
+            for (var I = 0; I < hour; I++)
+                RectSet(usedRect, horTix1, 3);
 
-            UsedRect = new List<Rectangle>();
-            for (var I = 0; I < Hour2; I++)
-                RectSet(UsedRect, HorTix2, 9);
+            usedRect = new List<Rectangle>();
+            for (var I = 0; I < hour2; I++)
+                RectSet(usedRect, horTix2, 9);
         }
 
         private static void RectSet(ICollection<Rectangle> usedRect, IReadOnlyList<Rectangle> minTix, int max)
         {
             while (true)
             {
-                var Rnd = new Random();
+                var rnd = new Random();
 
-                var Rect = minTix[Rnd.Next(0, max)];
-                if (usedRect.Contains(Rect))
+                var rect = minTix[rnd.Next(0, max)];
+                if (usedRect.Contains(rect))
                     continue;
-                Rect.Visibility = Visibility.Visible;
-                usedRect.Add(Rect);
+                rect.Visibility = Visibility.Visible;
+                usedRect.Add(rect);
                 break;
             }
         }
 
         private static void ClearTix(IEnumerable<Rectangle> index)
         {
-            foreach (var Rect in index)
-                Rect.Visibility = Visibility.Hidden;
+            foreach (var rect in index)
+                rect.Visibility = Visibility.Hidden;
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
